dc0de0ce527b42a4ba88cf6a8efaa99bc5c4939f
7Liberal7 <89695467+7Liberal7@users.noreply.github.com>
2022-01-22 22:52:15 +0500

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+// $(function () {
+//   $('.first-display__popup').magnificPopup({
+//     disableOn: 700,
+//     type: 'iframe',
+//     mainClass: 'mfp-fade',
+//     removalDelay: 1160,
+//     preloader: false,
+//     fixedContentPos: false
+//   });
+// })
